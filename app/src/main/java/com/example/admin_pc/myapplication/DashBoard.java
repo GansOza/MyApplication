9e1c2e6b190105a2ec22a407d6f1acdbb2732d08
GansOza <ganeshoza04@gmail.com>
2019-01-09 14:02:06 +0530

    updated part 1

---
+++ app/src/main/java/com/example/admin_pc/myapplication/DashBoard.java
@@ -0,0 +1,88 @@
+package com.example.admin_pc.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import org.w3c.dom.Text;
+
+public class DashBoard extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private GoogleApiClient mGoogleApiClient;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.support:
+                    mTextMessage.setText(R.string.support);
+                    return true;
+                case R.id.options:
+                    mTextMessage.setText(R.string.options);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        String ab = getIntent().getStringExtra("name");
+        String cd = getIntent().getStringExtra("email");
+
+
+
+        TextView textemail = (TextView) findViewById(R.id.textemail);
+        TextView textname = (TextView) findViewById(R.id.textname);
+
+        textemail.setText(cd);
+        textname.setText(ab);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+
+
+
+
+
+    }
+
+}
